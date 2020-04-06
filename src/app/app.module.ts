de016341ba0d525786cb0cb191a057faeb9a80a7
Bikcode <bikcodeh@gmail.com>
2020-04-06 08:37:39 -0500

    feature: agrega y configura modulo graphql y se ancla el app principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GraphqlModule } from './graphql/graphql.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+  BrowserModule,
+    AppRoutingModule,
+    GraphqlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
